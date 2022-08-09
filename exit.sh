18eba3706263f0d6cb5976e4d6e25c47156c09c0
Agnomilted <72987041+Agnomilted@users.noreply.github.com>
2022-08-09 18:25:42 +0300

    Add files via upload

--- exit.sh
+++ exit.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-a=$(echo -e 'logout\nrestart\npoweroff\nreboot' | rofi -dmenu -p 'Logout, restart bspwm, poweroff or reboot?');
+# a=$(echo -e 'logout\nrestart\npoweroff\nreboot' | rofi -dmenu -p 'Logout, restart bspwm, poweroff or reboot?');
+a=$(echo -e 'logout\nrestart\npoweroff\nreboot' | dmenu -l 4 -p 'Logout, restart bspwm, poweroff or reboot?');
 if [ $a = 'logout' ]
 then
 	bspc quit
@@ -9,8 +10,10 @@ then
 	bspc wm -r
 elif [ $a = 'poweroff' ]
 then
-	rofi -dmenu -password -p 'Please enter your user password' | sudo -S poweroff
+#	rofi -dmenu -password -p 'Please enter your user password' | sudo -S poweroff
+	poweroff
 elif [ $a = 'reboot' ]
 then
-	rofi -dmenu -password -p 'Please enter your user password' | sudo -S reboot
+#	rofi -dmenu -password -p 'Please enter your user password' | sudo -S reboot
+	reboot
 fi
--- xmenu.sh
+++ xmenu.sh
@@ -4,24 +4,26 @@ xmenu <<EOF | sh &
 Applications
 	IMG:$HOME/src/xmenu/icons/firefox.png	Web Browser	firefox
 	IMG:$HOME/src/xmenu/icons/gimp.png	GIMP	gimp
-	IMG:$HOME/src/xmenu/icons/discord.png	Discord	discord
+	IMG:$HOME/src/xmenu/icons/discord.png	Discord	discord-ptb
 File Browsers
 	Thunar	thunar
-	Ranger	st -e ranger $HOME
+	Ranger	$TERMINAL -e ranger $HOME
 Terminals
 	Gnome	gnome-terminal
 	st	st
+	default terminal	$TERMINAL
 Rofi	rofi -show drun
 System Configuration
-	Dwm	st -e vim $HOME/src/dwm/config.h
-	Xmenu	st -e vim $HOME/src/xmenu/xmenu.sh
+	Dwm	$TERMINAL -e $EDITOR $HOME/src/dwm/config.h
+	Xmenu	$TERMINAL -e $EDITOR $HOME/src/xmenu/xmenu.sh
 	LXAppearance	lxappearance
 	Bspwm
-		bspwmrc	st -e vim $HOME/.config/bspwm/bspwmrc
-		sxhkd	st -e vim $HOME/.config/bspwm/sxhkdrc
-		polybar	st -e vim $HOME/.config/polybar/config
-		xmenu	st -e vim $HOME/.config/bspwm/xmenu.sh
+		bspwmrc	$TERMINAL -e $EDITOR $HOME/.config/bspwm/bspwmrc
+		sxhkd	$TERMINAL -e $EDITOR $HOME/.config/bspwm/sxhkdrc
+		polybar	$TERMINAL -e $EDITOR $HOME/.config/polybar/config
+		xmenu	$TERMINAL -e $EDITOR $HOME/.config/bspwm/xmenu.sh
 Wallpaper Selector	select-wallpaper
+Bluetooth	blueman-manager
 
 Shutdown		poweroff
 Reboot			reboot
